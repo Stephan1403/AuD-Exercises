d44e0c5471e47533f605c8ddef950eac7f892cd0
Stephan <Stephanf2003>
2022-05-18 18:56:50 +0200

    added testing

--- Exersice7/main.cpp
+++ Exersice7/main.cpp
@@ -2,10 +2,13 @@
 #include "tabelle.cpp"
 
 int main(){
+    int store;
     Tabelle<int>* t = new Tabelle<int>();
 
     t->append(3);
     t->append(2);
-
+    t->get_node(store);
+    t->last();
+    t->get_node(store);
 
 }
