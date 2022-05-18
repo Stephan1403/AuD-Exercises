c2f525b05c5b448f0f9cbcf09571caed2e35a94b
Stephan <Stephanf2003>
2022-05-18 18:50:08 +0200

    fixed errors

--- Exersice7/main.cpp
+++ Exersice7/main.cpp
@@ -4,6 +4,8 @@
 int main(){
     Tabelle<int>* t = new Tabelle<int>();
 
+    t->append(3);
+    t->append(2);
 
-    
+
 }
--- Exersice7/tabelle.cpp
+++ Exersice7/tabelle.cpp
@@ -16,6 +16,10 @@ class Tabelle{
 //constructor and destructor
         Tabelle(){
             std::cout << "Created new table" << std::endl;
+            current = nullptr;
+            tail = nullptr;
+            head = nullptr;
+            length = 0;
         }
         ~Tabelle(){
             std::cout << "A new table has been created" << std::endl;
@@ -68,7 +72,7 @@ class Tabelle{
                 this->current = n;
             }else{
                 n->last = this->head;   //set this.last to last
-                this->head.next = n;    //set last.next to this
+                this->head->next = n;    //set last.next to this
 
                 this->head = n;         //the current node is now the head
                 this->current = n;
@@ -76,20 +80,122 @@ class Tabelle{
         }
 
         //set record
-        bool first();
-        bool last();
-        bool next();
-        bool previous();
+        bool first(){
+            if(this->length == 0)
+                return false;
+
+            this->current = this->head;
+            return true;
+        }
+
+        bool last(){
+            if(this->length == 0)
+                return false;
+
+            this->current = this->tail;
+            return true;
+        }
+
+        bool next(){
+            if(this->length == 0)
+                return false;
+
+            if(this->next == nullptr)
+                return false;
+
+            this->current = this->current->next;
+            return true;
+        }
+
+        bool previous(){
+            if(this->length == 0)
+                return false;
+
+            if(this->last == nullptr)
+                return false;
+
+            this->current = this->current->last;
+        }
 
 //delete, get, substitute
-        bool delete_node();
-        bool get_node(T &r);
-        bool set_node(T &r);
+        bool delete_node(){
+            if(this->length == 0)
+                return false;
+        
+            if(this->length == 0){
+                delete [] this->current;
+                this->current = nullptr;
+                this->tail = nullptr;
+                this->head = nullptr;
+                this->length = 0;
+                return true;
+            }
+
+            //current is tail
+            if(this->current == this->tail){
+                this->tail->next->last = nullptr;
+                this->tail = this->current->next;
+
+                //delete node
+                delete [] this->current;
+                this->length--;
+
+                //set current
+                this->current = this->tail;
+
+                return true;
+            }
+
+            //current is head
+            if(this->current == this->head){
+                this->head->last->next = nullptr;
+                this->head = this->current->last;
+
+                //delete node
+                delete [] this->current;
+                this->length--;
+
+                //set current
+                this->current = this->head;
+
+                return true;
+            }
+
+            //wether tail nor head
+            this->current->next->last = this->current->last;
+            this->current->last->next = this->current->next;
+
+
+            delete [] this->current;
+            this->length--;
+
+            //set current
+            this->current = this->head;
+
+            return true;
+        }
+
+        bool get_node(T &r){
+            if(this->length == 0)
+                return false;
+
+            r = this->current->val;
+            std::cout << "r= " << r << std::endl;
+            return true;
+        }
+
+        bool set_node(T &r){
+            if(this->length == 0)
+                return false;
+            
+            this->current->val = r;
+            return true;
+        }
 
     private:
-        node* current = nullptr;
-        node* tail = nullptr;
-        node* head = nullptr;
-        int length = 0;
+        node* current;
+        node* tail;
+        node* head;
+        int length;
 };
 
