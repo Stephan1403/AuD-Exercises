df6838dac0e65fb730d5ed8292c3c34e7503fcbc
Stephan <Stephanf2003>
2022-05-18 19:09:43 +0200

    testing

--- Exersice7/main.cpp
+++ Exersice7/main.cpp
@@ -5,10 +5,29 @@ int main(){
     int store;
     Tabelle<int>* t = new Tabelle<int>();
 
+    //set values
     t->append(3);
     t->append(2);
-    t->get_node(store);
+    t->append(4);
+
+    //move iterator
     t->last();
+    t->last();
+
+
+    t->insert(42);
+
+    t->next();
+    t->get_node(store);
+
+
+    t->next();
+
+    std::cout << "Node before delete: ";
+    t->get_node(store);
+    t->delete_node();
+
+    std::cout << "Node after delete: ";
     t->get_node(store);
 
 }
