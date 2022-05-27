bd08e117eaab93a96a8bc82197bd7f2bc1931a2d
Stephan <Stephanf2003>
2022-05-27 22:53:29 +0200

    implemented testing

--- Exersice8/main.cpp
+++ Exersice8/main.cpp
@@ -5,8 +5,8 @@
 template<class T>
 void insertionsSort(T* values, int len){
 
-
-for(int j, i=1; i<len)
+int i, j;
+for(i=1; i<len; i++){
 
     T an = values[i];
     j = i;
@@ -15,13 +15,26 @@ for(int j, i=1; i<len)
         values[j] = values[j-1];
         j--;
     }
-    a[j] = an;
+    values[j] = an;
+
+    //print values
+    std::cout << std::endl << "i=" << i << ": ";
+
+    for(int x=0; x<len; x++){
+        std::cout << values[x] << " ";
+    }
+}
+
 }
 
 
 
 int main(){
-    
+
+    int* a = new int[9];
+    a[0] = 0, a[1] = 10, a[2] = 20, a[3] = 1, a[4] = 11, a[5] = 21, a[6] = 2, a[7] = 12, a[8] = 22;
+
+    insertionsSort<int>(a, 9);
 
 
 }
