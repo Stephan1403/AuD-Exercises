1655b6793ff8c500db4c71935b48fc0c3b720bd2
Stephan <Stephanf2003>
2022-06-09 08:09:49 +0200

    updated testing

--- Exersice9/main.cpp
+++ Exersice9/main.cpp
@@ -7,13 +7,14 @@ template<class T>
 void exchangeSort(T* values, int len){
 
     // Testing
-    int comparisons = 0;
+    int comparisons = 0, runthroughs = 0;
 
     bool unsorted = true;                        //W hen false break from loop
     int i, j; 
     for(i=0; i<len-1 && unsorted; i++){
 
         unsorted = false;                        // Expect the array to be sorted for the next run
+        runthroughs++;
 
         for(j=len-2; j>=i; j--){
 
@@ -36,16 +37,25 @@ void exchangeSort(T* values, int len){
     }
 
     std::cout << "\nRequired " << comparisons << " comparisons" << std::endl;
+    std::cout << "Required " << runthroughs << " runthroughs" << std::endl;
+
 }
 
 
 
-
-
 int main(){
-    int* a = new int[9];
-    a[0] = 0, a[1] = 10, a[2] = 20, a[3] = 1, a[4] = 11, a[5] = 21, a[6] = 2, a[7] = 12, a[8] = 22;
+    int* a = new int[3];
+    a[0] = 0, a[1] = 10, a[2] = 20;
 
-    exchangeSort<int>(a, 9);
+    exchangeSort<int>(a, 3);
 
+    int* b = new int[6];
+    b[0] = 0, b[1] = 10, b[2] = 20, b[3] = 1, b[4] = 11, b[5] = 21;
+
+    exchangeSort<int>(b, 6);
+
+    int* c= new int[9];
+    c[0] = 0, b[1] = 10, c[2] = 20, c[3] = 1, c[4] = 11, c[5] = 21, c[6] = 2, c[7] = 12, c[8] = 22;
+
+    exchangeSort<int>(c, 9);
 }
